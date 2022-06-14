a8a73fb2c00cd46b274a24638513c64cdc1fa7ab
Oluwasegun Ige <40037490+Tshergzeh@users.noreply.github.com>
2022-06-14 18:45:38 +0100

    Implemented GeniusComputerPlayer

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from player import GenuisComputerPlayer, HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
     def __init__(self):
@@ -82,13 +82,25 @@ def play(game, x_player, o_player, print_game=True):
             
             letter = 'O' if letter == 'X' else 'X'
 
-        time.sleep(0.8)  
+        # time.sleep(0.8)  
 
     if print_game:
         print("It's a tie")
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(1000):
+        x_player = RandomComputerPlayer('X')
+        o_player = GenuisComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+
+    print(f'After 1000 iterations, we see {x_wins} X wins, {o_wins} O wins and {ties} ties.')
